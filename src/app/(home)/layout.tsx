e91b02486c4a053f6931097e48b59da201af873d
sojibulislamrana <sojibulislamrana.dev@gmail.com>
2026-04-02 02:04:37 +0600

    feat: Footer and Home Layout

--- src/app/(home)/layout.tsx
+++ src/app/(home)/layout.tsx
@@ -1,18 +1,18 @@
-
+import { Footer } from "./Footer";
 import { Navbar } from "./navbar";
 
-
 interface props {
-    children: React.ReactNode;
+  children: React.ReactNode;
 }
 
 const Layout = ({ children }: props) => {
-    return (
-        <div className="flex flex-col min-h-screen">
-            <Navbar />
-            {children}
-        </div>
-    )
-}
+  return (
+    <div className="flex flex-col min-h-screen">
+      <Navbar />
+      <div className="flex-1 bg-[#f4f4f0]">{children}</div>
+      <Footer />
+    </div>
+  );
+};
 
 export default Layout;
